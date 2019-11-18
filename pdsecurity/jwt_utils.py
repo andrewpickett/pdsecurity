227e2cda6b7a77902b65bf8a91aedd41dc3e115f
Andrew Pickett <picketta@gmail.com>
2019-11-18 14:11:49 -0500

    Initial commit

---
+++ pdsecurity/jwt_utils.py
@@ -0,0 +1,74 @@
+import datetime
+import functools
+
+from werkzeug.wrappers import Request
+from werkzeug.exceptions import abort
+import jwt
+
+from pdsecurity_config import pdsecurity_config
+
+
+def protect_with_jwt(f):
+    """
+    This is a decorator function for validating requests from the user to ensure they have a valid JWT token
+    on them. If the JWT doesn't exist or is invalid, then flask will abort the request with a 401 status code.
+
+    :param f: The function to decorate
+    :return: The decorator for the function
+    """
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        if not check_jwt_filter(Request):
+            abort(401)
+
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def check_jwt_filter(request):
+    """
+    Validates that the request contains a valid JWT token.
+
+    :param request: The request from the user to validate.
+    :return: True if the request has a valid JWT token, False otherwise.
+    """
+    try:
+        auth_token = _get_jwt_token_from_request(request)
+        auth_request = _parse_jwt_token(auth_token)
+
+        if auth_request and auth_request["iss"] == pdsecurity_config['jwt']['issuer'] and datetime.datetime.now() < datetime.datetime.fromtimestamp(auth_request["exp"]):
+            # TODO: Save auth to some context? or at least just allow them in.
+            pass
+        else:
+            raise KeyError("Couldn't parse claims and/or expired token detected.")
+    except KeyError:
+        return False
+
+    return True
+
+
+def get_jwt_object_from_request(request):
+    return _parse_jwt_token(_get_jwt_token_from_request(request))
+
+
+def _get_jwt_token_from_request(request):
+    header = request.headers["Authorization"]
+    if header.startswith('Bearer '):
+        return header.replace('Bearer ', '')
+    else:
+        raise KeyError("No valid JWT token available on request.")
+
+
+def _parse_jwt_token(jwt_token):
+    """
+    Decode the encoded JWT token into the object.
+
+    :param jwt_token: The JWT token to decode
+    :return: The decoded JWT object
+    """
+    try:
+        return jwt.decode(jwt_token, pdsecurity_config['jwt']['secret_key'], algorithms=[pdsecurity_config['jwt']['algorithm']])
+    except jwt.InvalidTokenError:
+        return None
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pdsecurity",
+    version="0.0.2",
+    author="Andrew J. Pickett",
+    author_email="picketta@gmail.com",
+    description="Simple package containing basic security classes/code for bcrypt and JWT auth",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andrewpickett/pdsecurity",
+    packages=setuptools.find_packages(),
+    # py_modules=['pdsecurity_config'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
